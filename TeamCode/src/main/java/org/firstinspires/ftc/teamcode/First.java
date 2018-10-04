0934cb989bb8b51b6e9fa17b38dbfdbf72b9bf1a
20jobea <aaronajobe@gmail.com>
2018-10-04 07:27:33 -0700

    actually made the test class work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/First.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/First.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name="Test", group="Test")
-public class First extends LinearOpMode {
+public class First extends OpMode {
     @Override
-    public void runOpMode() {
+    public void init() {
+        telemetry.addData("Does ", "this work?");
+    }
+    @Override
+    public void loop() {
         telemetry.addData("Does ", "this work?");
     }
 }
