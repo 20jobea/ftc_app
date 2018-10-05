3692902a7fec5f62efb5bce3fa2fe51b0133b06a
20jobea <aaronajobe@gmail.com>
2018-10-04 19:29:38 -0700

    insert good commit message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,6 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Autonomous(name="Test", group="Test")
 public class Test extends OpMode {
+    public DcMotor motor  = hardwareMap.get(DcMotor.class, "Best");
+    @Override
+    public void init() {
+        telemetry.addData("Does ", "this work?");
+    }
+    @Override
+    public void loop() {
+        telemetry.addData("Does ", "this work?");
+        motor.setPower(0.1);
+    }
 }
