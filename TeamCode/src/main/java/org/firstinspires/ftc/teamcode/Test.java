2bbcf147ef310976e702eedb85b59979ffb14497
20jobea <aaronajobe@gmail.com>
2018-10-09 18:58:12 -0700

    Finishes a class that makes sure everything works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,19 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.motors.RevRobotics40HdHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name="Test", group="Test")
 public class Test extends OpMode {
-    public DcMotor motor  = hardwareMap.get(DcMotor.class, "Best");
+    //public RevRobotics40HdHexMotor motor  = this.hardwareMap.get(RevRobotics40HdHexMotor.class, "Test");
+    public DcMotor motor;
     @Override
     public void init() {
         telemetry.addData("Does ", "this work?");
+        motor = this .hardwareMap.get(DcMotor.class, "Test");
     }
     @Override
     public void loop() {
         telemetry.addData("Does ", "this work?");
         motor.setPower(0.1);
+         try {
+             Thread.sleep(500);
+         } catch (Exception e) {
+
+         }
+         motor.setPower(0);
     }
 }
