5eef136127a34af8298665c37a4c92c0975c6ae0
Aaron <arjunjobe@me.com>
2018-11-07 19:49:13 -0800

    Adds a bunch of files to start with (Auto and Hardware)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCrater.java
@@ -0,0 +1,6 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Blue Crater", group="Auto")
+public class AutoBlueCrater extends Autonomous3058 {
+  
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedDepot.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode;
+
+@Autonomous(name="Red Depot", group="Auto")
+public class AutoRedDepot extends Autonomous3058 {
+
+}
