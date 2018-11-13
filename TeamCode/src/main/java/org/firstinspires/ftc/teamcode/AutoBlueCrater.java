e5dc754d9158569f030b78305075b973ae34cc95
20jobea <34043362+20jobea@users.noreply.github.com>
2018-11-13 14:17:47 -0800

    put it into something to test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueCrater.java
@@ -2,12 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="Blue Crater", group="Auto")
+@Autonomous(name="Blue Crater (test)", group="Auto")
 public class AutoBlueCrater extends Autonomous3058 {
     @Override
     public void runOpMode() {
         init(hardwareMap);
-        //land();
-        move(100000, 0.6);
+        land();
+        move(10000, 0.6);
     }
 }
