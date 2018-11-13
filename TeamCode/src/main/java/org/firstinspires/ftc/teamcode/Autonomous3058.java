5e2f765dbae87051a934072214d3b4ab48ab7b56
20jobea <34043362+20jobea@users.noreply.github.com>
2018-11-13 14:06:44 -0800

    fix the move thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous3058.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous3058.java
@@ -26,28 +26,39 @@ abstract public class Autonomous3058 extends LinearOpMode {
     }
     public void land() {
         robot.getMotor(ImplHardware.Motor.LIFT).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.getMotor(ImplHardware.Motor.LIFT).setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.setMotorPower(ImplHardware.Motor.LIFT, 0.3);
-        //Change THIS NUMBER!!!!!!!!!!!!!!!!!!!!
-        while (robot.getMotor(ImplHardware.Motor.LIFT).getCurrentPosition() < 10000) {
-            telemetry.addData("Lift Position", robot.getMotor(ImplHardware.Motor.LIFT).getCurrentPosition());
+        if (opModeIsActive()) { 
+            robot.getMotor(ImplHardware.Motor.LIFT).setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.setMotorPower(ImplHardware.Motor.LIFT, 0.3);
+            robot.getMotor(ImplHardware.Motor.LIFT).setTargetPosition(1000);
+            //Change THIS NUMBER!!!!!!!!!!!!!!!!!!!! ^^^^^^^^^^^^^^^^^^
+            while (opModeIsActive() && robot.getMotor(ImplHardware.Motor.LIFT).isBusy()) {
+                telemetry.addData("Lift Position", robot.getMotor(ImplHardware.Motor.LIFT).getCurrentPosition());
+            }
+            robot.setMotorPower(ImplHardware.Motor.LIFT, 0);
+            robot.getMotor(ImplHardware.Motor.RIGHT).setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-        robot.setMotorPower(ImplHardware.Motor.LIFT, 0);
 
     }
     public void move(double distance, double speed) {
+        distance *= ImplHardware.INCHES_TO_ENCODER;
         robot.getMotor(ImplHardware.Motor.LEFT).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.getMotor(ImplHardware.Motor.RIGHT).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.getMotor(ImplHardware.Motor.RIGHT).setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.getMotor(ImplHardware.Motor.LEFT).setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.setMotorPower(ImplHardware.Motor.LEFT, speed);
-        robot.setMotorPower(ImplHardware.Motor.RIGHT, speed);
-        distance *= ImplHardware.INCHES_TO_ENCODER;
-        while ((robot.getMotor(ImplHardware.Motor.LEFT).getCurrentPosition() < distance) && (robot.getMotor(ImplHardware.Motor.LEFT).getCurrentPosition() < distance)) {
-            telemetry.addData("Left Position", robot.getMotor(ImplHardware.Motor.LEFT).getCurrentPosition());
-            telemetry.addData("Right Position", robot.getMotor(ImplHardware.Motor.RIGHT).getCurrentPosition());
-        }
-        robot.setMotorPower(ImplHardware.Motor.LEFT, 0);
-        robot.setMotorPower(ImplHardware.Motor.RIGHT, 0);
+        if (opModeIsActive()) {
+            //robot.getMotor(ImplHardware.Motor.RIGHT).getCurrentPosition() + 
+            robot.getMotor(ImplHardware.Motor.RIGHT).setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.getMotor(ImplHardware.Motor.LEFT).setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.getMotor(ImplHardware.Motor.RIGHT).setTargetPosition(distance);
+            robot.getMotor(ImplHardware.Motor.LEFT).setTargetPosition(distance);
+            robot.setMotorPower(ImplHardware.Motor.LEFT, speed);
+            robot.setMotorPower(ImplHardware.Motor.RIGHT, speed);
+            while (opModeIsActive() && robot.getMotor(ImplHardware.Motor.RIGHT).isBusy() && robot.getMotor(ImplHardware.Motor.RIGHT).isBusy()) {
+                telemetry.addData("Left: ", robot.getMotor(ImplHardware.Motor.LEFT).getCurrentPosition());
+                telemetry.addData("Right: ", robot.getMotor(ImplHardware.Motor.LEFT).getCurrentPosition());
+            }
+            robot.setMotorPower(ImplHardware.Motor.LEFT, 0);
+            robot.setMotorPower(ImplHardware.Motor.RIGHT, 0);
+            robot.getMotor(ImplHardware.Motor.LEFT).setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.getMotor(ImplHardware.Motor.RIGHT).setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }    
     }
 }
